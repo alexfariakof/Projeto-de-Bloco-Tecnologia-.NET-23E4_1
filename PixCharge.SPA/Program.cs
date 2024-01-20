f8336fb48b9ecc5232b403c4063a84315e1d0bc5
Ribeiro <alexfariakof@gmail.com>
2024-01-20 18:34:38 -0300

    Configurações Docker

--- PixCharge.SPA/Program.cs
+++ PixCharge.SPA/Program.cs
@@ -1,18 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(c =>
+{
+    c.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+
+    });
+});
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
+    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors();
     app.UseHsts();
 }
 
+if (app.Environment.IsStaging())
+{
+    app.Urls.Add("http://0.0.0.0:2000");
+    app.Urls.Add("https://0.0.0.0:2001");
+}
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
